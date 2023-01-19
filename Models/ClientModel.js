b9c5325e1f0591d72e557280563cd18ba0a0a6b1
birendra <sangam9060@gmail.com>
2023-01-19 12:23:23 +0530

    fifth

--- Models/ClientModel.js
+++ Models/ClientModel.js
@@ -1,41 +1,80 @@
 const mongoose = require("mongoose");
 
-const clientSchema = new mongoose.Schema({
+const clientSchema = new mongoose.Schema(
+  {
     agentname: {
-        type: String,
-        required: [true, "Please enter agentname"],
-      },
-      closername: {
-        type: String,
-        required: [true, "Please enter closername"],
-      },
-      customername: {
-        type: String,
-        required: [true, "Please enter customername"],
-      },
-      address: {
-        type: String,
-        required: [true, "Please enter address"],
-      },
+      type: String,
+      required: [true, "Please enter agentname"],
+    },
+    closername: {
+      type: String,
+      required: [true, "Please enter closername"],
+    },
+    customername: {
+      type: String,
+      required: [true, "Please enter customername"],
+    },
+    address: {
+      type: String,
+      required: [true, "Please enter address"],
+    },
 
-  city: {
-    type: String,
-    required: [true, "Please enter city"],
-  },
+    city: {
+      type: String,
+      required: [true, "Please enter city"],
+    },
 
-  state: {
-    type: String,
-    required: [true, "Please enter state"],
-  },
+    state: {
+      type: String,
+      required: [true, "Please enter state"],
+    },
+    zipcode: {
+      type: String,
+      required: [true, "Please enter zipcode"],
+    },
+    phonenumber: {
+      type: String,
+      required: [true, "Please enter phonenumber"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please enter email"],
+    },
+    bank: {
+      type: String,
+      required: [true, "Please enter bank"],
+    },
+    routing: {
+      type: String,
+      required: [true, "Please enter routing"],
+    },
+    accountnumber: {
+      type: String,
+      required: [true, "Please enter accountnumber"],
+    },
+    ssn: {
+      type: String,
+      required: [true, "Please enter ssn"],
+    },
+    status: {
+      type: String,
+      required: [true, "Please enter status"],
+    },
+    comments: {
+      type: String,
+      required: [true, "Please enter comments"],
+    },
+    date: {
+      type: date,
+      default:date,
+      
+    },
 
-  createdAt: {
-    type: Date,
-    default: Date.now,
+  
   },
-},
-{
-    timestamps:true,
-}
+  {
+    timestamps: true,
+  }
 );
 
 module.exports = mongoose.model("Client", clientSchema);
